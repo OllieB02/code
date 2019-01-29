8687dbee24f6559b8434bd6f6d20758be51854ed
OllieB02 <47144078+OllieB02@users.noreply.github.com>
2019-01-29 12:16:43 +0000

    Add files via upload

---
+++ Highscores.py
@@ -0,0 +1,14 @@
+#Highscores
+
+##highScores = [ [12,18] , [23,5], [9,16] ]
+##highScores.append([20,20])
+##print(highScores)
+##print(highScores[1][0])
+
+salary = [ ["Steve",35000] , ["Alice",47000] , ["Greg",40000] , ["Spencer", 20000] ]
+length = len(salary)
+for i in range(length):
+    print(salary[0][1])
+
+print(salary[0][0])
+
